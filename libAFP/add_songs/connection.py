c05b7438f5ad078355d103722000afa20bec7963
Vikesh Khanna <khanna.vikesh@gmail.com>
2011-09-13 02:03:03 +0530

    Committing gitignore

--- libAFP/add_songs/connection.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import MySQLdb
-
-
-conn = MySQLdb.connect (host = "localhost",
-                        user = "root",
-                        passwd = "rishabh",
-                        db = "btp")
--- libAFP/gui/test.py
+++
@@ -1,6 +0,0 @@
-from Tkinter import *
-root = Tk()
-w = Label(root, text="Hello, world!")
-w.pack()
-root.mainloop()
-
